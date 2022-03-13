81c8b8f2d31fbe831af92cf31ed4b95df07e9f9e
mofasy <sayed.mfahad@gmail.com>
2022-03-13 17:41:41 +0400

    2 files modified

--- src/app/App.js
+++ src/app/App.js
@@ -1,23 +1,22 @@
-import React from "react";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import About from "../components/About";
-import SignUp from "../components/SignUp";
-import Articles from "../components/Articles";
-import Article from "../components/Article";
-import Categories from "../components/Categories";
-import Author from "../components/Author";
-import Profile from "../components/Profile";
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import About from '../components/About';
+import SignUp from '../components/SignUp';
+import Articles from '../components/Articles';
+import Article from '../components/Article';
+import Categories from '../components/Categories';
+import Author from '../components/Author';
+import Profile from '../components/Profile';
 
-import "./App.css";
+import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <Header />
-      <main>
-        {/* Add Routes here! */}
-      </main>
+      <main>{/* Add Routes here! */}</main>
       <Footer />
     </div>
   );
