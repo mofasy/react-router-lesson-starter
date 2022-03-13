6a4afda7d0ce47359a875afe02bc095a89e4356b
mofasy <sayed.mfahad@gmail.com>
2022-03-13 20:09:49 +0400

    2 files modified

--- src/app/App.js
+++ src/app/App.js
@@ -10,15 +10,31 @@ import Author from '../components/Author';
 import Profile from '../components/Profile';
 
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
-      <main>{/* Add Routes here! */}</main>
+      <main>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/sign-up">
+          <SignUp />
+        </Route>
+        <Route path="/articles">
+          <Articles />
+        </Route>
+        <Route path="/categories">
+          <Categories />
+        </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+      </main>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
